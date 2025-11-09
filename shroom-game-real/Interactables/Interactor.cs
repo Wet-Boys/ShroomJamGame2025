8bf936c2847f4e1f5d5d7e59a6fb73a8ca99163f
Rune580 <runemaster580@gmail.com>
2025-11-09 13:57:47 -0500

    interact label text and visibility

--- shroom-game-real/Interactables/Interactor.cs
+++ shroom-game-real/Interactables/Interactor.cs
@@ -7,6 +7,7 @@ namespace ShroomGameReal.Interactables;
 public partial class Interactor : RayCast3D
 {
     private PlayerController _player;
+    private InteractLabel _interactLabel;
     private IInteractable _currentInteractable;
     
     [Export]
@@ -15,12 +16,22 @@ public partial class Interactor : RayCast3D
     public override void _Ready()
     {
         _player = GetOwner<PlayerController>();
+        _interactLabel = GetNode<InteractLabel>("%Interact Label");
+
+        UpdateLabelVisibility();
     }
 
     public override void _PhysicsProcess(double delta)
     {
         if (GlobalGameState.Instance.IsMainPaused)
             return;
+
+        if (_currentInteractable is not null && !_currentInteractable.CanInteract)
+        {
+            _currentInteractable.OnDeselected();
+            _currentInteractable = null;
+            return;
+        }
         
         if (!IsColliding() || !Active)
         {
@@ -36,9 +47,39 @@ public partial class Interactor : RayCast3D
         var collider = GetCollider();
         if (collider is not IInteractable interactable)
             return;
+        
+        if (!interactable.CanInteract)
+            return;
+
+        if (interactable != _currentInteractable)
+            _interactLabel.SetText(interactable.GetInteractText());
 
         interactable.OnSelected();
         _currentInteractable = interactable;
+    }
+
+    public override void _Process(double delta)
+    {
+        UpdateLabelVisibility();
+
+        if (_currentInteractable is null || !Active || GlobalGameState.Instance.IsMainPaused)
+            return;
+
+        var (screenMin, screenMax) = _currentInteractable.GetScreenBounds();
+        var screenCenter = screenMin.Lerp(screenMax, 0.5f);
+        
+        
+    }
+
+    private void UpdateLabelVisibility()
+    {
+        if (_currentInteractable is null)
+        {
+            _interactLabel.Visible = false;
+            return;
+        }
+        
+        _interactLabel.Visible = _currentInteractable.CanInteract && Active;
     }
 
     public override void _Input(InputEvent @event)
