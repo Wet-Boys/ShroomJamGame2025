ba87f5ab1d44c892a87f6bd2e6f57a2ae645125d
Nunchuk <49164897+ToastedOven@users.noreply.github.com>
2025-11-11 16:29:48 -0500

    fixed floor

--- shroom-game-real/scenes/dream/transition/DreamTransition.cs
+++ shroom-game-real/scenes/dream/transition/DreamTransition.cs
@@ -5,4 +5,14 @@ public partial class DreamTransition : Control
 {
     [Export] private AnimatedSprite2D _background;
     [Export] private Label _text;
+    [Export] private AnimationPlayer _animator;
+
+    public override void _Ready()
+    {
+        base._Ready();
+        _background.Play();
+        _text.Text = "Amongus";
+        _animator.Play("Transition");
+        _animator.Stop();
+    }
 }
