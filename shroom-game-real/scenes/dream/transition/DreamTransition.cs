7adc08bcfaa162dd28b627efa2c37e64fc25a706
Nunchuk <49164897+ToastedOven@users.noreply.github.com>
2025-11-11 16:59:58 -0500

    holy shit, fuck our lives

--- shroom-game-real/scenes/dream/transition/DreamTransition.cs
+++ shroom-game-real/scenes/dream/transition/DreamTransition.cs
@@ -6,13 +6,21 @@ public partial class DreamTransition : Control
     [Export] private AnimatedSprite2D _background;
     [Export] private Label _text;
     [Export] private AnimationPlayer _animator;
+    public static DreamTransition instance;
 
     public override void _Ready()
     {
         base._Ready();
         _background.Play();
-        _text.Text = "Amongus";
+        _text.Text = "";
+        _background.Visible = false;
+        instance = this;
+    }
+
+    public void PlayWithText(string text)
+    {
+        _background.Visible = true;
+        _text.Text = text;
         _animator.Play("Transition");
-        _animator.Stop();
     }
 }
